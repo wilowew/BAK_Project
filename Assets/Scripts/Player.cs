e1460dbe665418f1017341b1829fe18fe92d157f
ItIsAnyx <144255113+ItIsAnyx@users.noreply.github.com>
2024-09-21 01:11:00 +0300

    Изменена механика движения персонажа, добавлен первичный спрайт главного героя

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float movingSpeed = 5f;
     private Rigidbody2D rb;
 
     private void Awake()
@@ -13,29 +14,8 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1f;
-        }
-
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1f;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1f;
-        }
-
-        rb.MovePosition(rb.position + inputVector * Time.fixedDeltaTime);
-
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        inputVector = inputVector.normalized;
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
     }   
 }
