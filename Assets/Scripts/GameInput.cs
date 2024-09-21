ee09dd02ea79203580275f79ce9790347d93d207
Kirill Chertkov <144255468+Kirill74545@users.noreply.github.com>
2024-09-21 17:44:55 +0300

    Добавлен поворот персонажа в сторону курсора

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
@@ -18,5 +19,11 @@ public class GameInput : MonoBehaviour
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
         return inputVector;
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        Vector3 mousePos = Mouse.current.position.ReadValue();
+        return mousePos;
     }
 }
