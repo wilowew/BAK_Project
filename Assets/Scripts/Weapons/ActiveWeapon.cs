320f6e31b652d83c951ab16900ce63a489559c1f
Kirill Chertkov <144255468+Kirill74545@users.noreply.github.com>
2024-10-13 00:11:14 +0300

    Исправлено положение меча при повороте в другую сторону; добавлена анимация взмаха меча

--- Assets/Scripts/Weapons/ActiveWeapon.cs
+++ Assets/Scripts/Weapons/ActiveWeapon.cs
@@ -12,8 +12,28 @@ public class ActiveWeapon : MonoBehaviour
         Instance = this;
     }
 
+    private void Update()
+    {
+        FollowMousePosition();
+    }
+
     public Sword GetActiveWeapon()
     {
         return sword;
+    }
+
+    private void FollowMousePosition()
+    {
+        Vector3 mousePos = GameInput.Instance.GetMousePosition();
+        Vector3 playerPosition = Player.Instance.GetPlayerScreenPosition();
+
+        if (mousePos.x < playerPosition.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
--- Assets/Scripts/Weapons/SwordVisual.cs
+++ Assets/Scripts/Weapons/SwordVisual.cs
@@ -26,4 +26,6 @@ public class SwordVisual : MonoBehaviour
     {
         animator.SetTrigger(ATTACK);
     }
+
+
 }
