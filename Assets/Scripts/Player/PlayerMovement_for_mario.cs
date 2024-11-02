9550b76e58579a818ceee2c420d61a2d893aadd6
wilowew <peoplechtoto@gmail.com>
2024-11-02 18:01:55 +0300

    Исправлены ускорения при прыжке от боковых коллайдеров, кирпичи теперь отталкиваются башкой

--- Assets/Scripts/Player/PlayerMovement_for_mario.cs
+++ Assets/Scripts/Player/PlayerMovement_for_mario.cs
@@ -2,56 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMoving_for_mario : MonoBehaviour
-{
-
+public class PlayerMoving_for_mario : MonoBehaviour {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     [SerializeField] private Vector3 groundCheck;
     [SerializeField] private LayerMask groundMask;
 
     private Vector3 input;
-    private bool IsRunning;
-    private bool isGraunded;
+    private bool isRunning;
+    private bool isGrounded;
 
     private Rigidbody2D rigidbody;
     private Animation_for_mario animations;
     [SerializeField] private SpriteRenderer characterSprite;
 
-    private void Start()
-    {
+    private void Start() {
         rigidbody = GetComponent<Rigidbody2D>();
         animations = GetComponentInChildren<Animation_for_mario>();
-        }
+    }
 
-    private void Update()
-    {
+    private void Update() {
         Move();
         CheckGround();
-        if (Input.GetKeyDown(KeyCode.W) && isGraunded) {
-                Jump();               
-            }
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded) {
+            Jump();
+        }
     }
 
     private void CheckGround() {
-        float radius = 0.2f;
-        Vector3 rayStartPosition = transform.position + groundCheck;
+        Vector2 rayStartPosition = (Vector2)(transform.position + new Vector3(0, -0.15f, 0));
+        RaycastHit2D hit = Physics2D.Raycast(rayStartPosition, Vector2.down, 0.2f, groundMask);
+        isGrounded = hit.collider != null;
+    }
 
-        isGraunded = Physics2D.OverlapCircle(rayStartPosition, radius, groundMask) != null;
-        }
+
+
 
     private void Move() {
         input = new Vector2(Input.GetAxis("Horizontal"), 0);
         transform.position += input * speed * Time.deltaTime;
-        IsRunning = input.x != 0 ? true : false;
-        if (input.x != 0) {
-            characterSprite.flipX = input.x > 0 ? false : true;
-            }
-        animations.IsRunning = IsRunning;
+        isRunning = input.x != 0;
 
+        if (input.x != 0) {
+            characterSprite.flipX = input.x < 0;
         }
+        animations.IsRunning = isRunning;
+    }
+
     private void Jump() {
-        rigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-        }
-     
+        rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
 }
